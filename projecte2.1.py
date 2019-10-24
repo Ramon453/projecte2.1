5f26d45609e70d46a9001362f1b98f804699a485
Ramon453 <rbringaspol@iesbinissalem.net>
2019-10-24 12:52:44 +0200

    Primer commit

--- projecte2.1.py
+++ projecte2.1.py
@@ -1,2 +1,26 @@
-print("Introdueix la distància en cenrimetres")
-centimetre= input()
+print("Digues quina operació vols realitzar (suma | resta | multiplicacio | divisio) ")
+operacio = input()
+print("Introdueix dos nombres enters")
+enter1= input()
+enter2= input()
+
+if (operacio == "suma"):
+    print(int(enter1)+int(enter2))
+if (operacio == "resta"):
+    print(int(enter1)-int(enter2))
+if (operacio=="divisio"):
+    print(int(enter1)/int(enter2))
+if (operacio=="multiplicacio"):
+    print(int(enter1)*int(enter2))
+
+
+
+
+
+
+
+
+
+
+
+
